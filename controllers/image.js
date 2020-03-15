61b85ce253581db59de0ae714c465697f4064f53
Andronikus <tiagoafroque@gmail.com>
2020-03-15 00:06:42 +0000

    fix response object on image handler

--- controllers/image.js
+++ controllers/image.js
@@ -1,14 +1,14 @@
 const clarifai = require('clarifai');
 
 const app = new Clarifai.App({
- apiKey: process.env.CLARIFAI_API
+	apiKey: process.env.CLARIFAI_API
 });
 
 const handleImageURL = (req, res) => {
-	const {input} = req.body;
-	app.models.predict(Clarifai.FACE_DETECT_MODEL,input)
+	const { input } = req.body;
+	app.models.predict(Clarifai.FACE_DETECT_MODEL, input)
 		.then(response => {
-			if(!response){
+			if (!response) {
 				return res.status(400).json('Ops! Something went wrong with image detection');
 			}
 
@@ -23,11 +23,11 @@ const handleImageURL = (req, res) => {
 const handleImage = (db) => (req, res) => {
 	const { id } = req.body;
 	db('users')
-  		.where('id', '=', id)
-  		.increment('entries', 1)
-  		.returning('entries')
-  		.then(data => data.length > 0 ? res.json( {rank:data[0]} ) : res.status('404').json('cannot found user id'))
-  		.catch(err => res.status('400').json('updated failed!'))
+		.where('id', '=', id)
+		.increment('entries', 1)
+		.returning('entries')
+		.then(data => data.length > 0 ? res.json({ rank: data[0] }) : res.status('404').json('cannot found user id'))
+		.catch(err => res.status('400').json('updated failed!'))
 }
 
 module.exports = {
