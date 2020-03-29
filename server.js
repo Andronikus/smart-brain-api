ab5628792ca9d70e8548a89c44e7bb4dcbd939f2
Andronikus <tiagoafroque@gmail.com>
2020-03-29 23:06:08 +0100

    add nginx to docker-compose + add nginx config

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ app.use(morgan('combined'));
 
 //routes
 app.get('/', (req, res) => res.json("oh yeah. I am online"));
+app.get('/favicon.ico', (req, res) => res.json("oh yeah. favicon.ico"));
 app.post('/signin', signin.handleSignInWithAuthentication(db, bcrypt));
 app.post('/register', register.handleRegister(db, bcrypt));
 app.get('/profile/:id', auth.authorize, profile.handleProfileGetByID(db));
